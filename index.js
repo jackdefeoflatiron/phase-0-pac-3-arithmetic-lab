2f4302f273fdb58d57234e33613380f017e58269
John DeFeo <jackd3243@gmail.com>
2021-09-29 00:26:57 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function add(){
+    1 + 80; //=> 81
+}
+function subtract(){
+    60 - 40; //=> 20
+}
+function multiply(){
+    2 * 3.4; //=> 6.8
+}
+function divide(){
+    5.0 / 2.5; //=> 2
+}
+function add(a,b){
+    a + b; //=> 562
+}
+function subtract(a,b){
+    a - b; //=> -208
+}
+function multiply(a,b){
+    a * b; //=> 41870
+}
+function divide(a,b){
+    a / b; //=> 7.661016949152542
+}
+function add(a,b)
